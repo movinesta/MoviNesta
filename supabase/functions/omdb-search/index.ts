261c27f1597ce3d9f0ad130bdca9fc22a0d7b975
movinesta <movinesta@gmail.com>
2025-11-27 01:44:55 +0300

    Add allowed methods to omdb-search CORS headers

--- supabase/functions/omdb-search/index.ts
+++ supabase/functions/omdb-search/index.ts
@@ -8,6 +8,7 @@ const OMDB_API_KEY = Deno.env.get("OMDB_API_KEY");
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "POST, OPTIONS",
   "Access-Control-Allow-Headers":
     "authorization, x-client-info, apikey, content-type",
 };
