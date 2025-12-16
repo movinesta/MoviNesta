904a79b3091b087b7e1790172cff0be13e2ba376
movinesta <movinesta@gmail.com>
2025-12-16 21:10:22 +0300

    Update schema.sql

--- supabase/schema.sql
+++ supabase/schema.sql
@@ -1,3 +1,6 @@
+-- WARNING: This schema is for context only and is not meant to be run.
+-- Table order and constraints may not be valid for execution.
+
 CREATE TABLE public.activity_events (
   id uuid NOT NULL DEFAULT gen_random_uuid(),
   created_at timestamp with time zone NOT NULL DEFAULT now(),
